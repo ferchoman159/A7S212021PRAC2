d5c29073eb2cd97284968f9c3aa54bf3c93b217a
Fernando Villanueva Mayorga <71373619+ferchoman159@users.noreply.github.com>
2020-10-01 14:07:35 -0500

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/isc/fvm/a7s212021prac2/PrincipalActivity.java
@@ -0,0 +1,14 @@
+package mx.edu.tesoem.isc.fvm.a7s212021prac2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PrincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal2);
+    }
+}
